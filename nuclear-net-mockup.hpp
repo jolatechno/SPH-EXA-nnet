ab397e29ad184a0b6bc465f360270c6ff7a14fc3
jolatechno <joseph.touzet@gmail.com>
2022-04-11 11:04:21 +0200

    first commit

---
+++ nuclear-net-mockup.hpp
@@ -0,0 +1,324 @@
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <unsupported/Eigen/CXX11/Tensor>
+
+namespace nnet {
+	/* -------------------
+	Iteration protocol (r : desintegration rates, dr/dT, fi : fission rates, dfi="dfi/dT", fu : fusion rates, dfu="dfu/dT", Q : mass excedent, rho : specific heat, M : molar masses):
+		
+		// if needed, convert to mols
+		masses_to_mols(Y, M);
+
+		// if needed, rework desintegration rates 
+		//to insure that the equation is dY/dt = r*Y
+		auto r_included = desintegration_rate_to_first_order(r);
+		auto dr_included = desintegration_rate_to_first_order(dr);
+
+		// add fission rates to desintegration rates
+		r_included += nnet::fission_to_desintegration_rates(fi, Y);
+		dr_included += nnet::fission_to_desintegration_rates(dfi, Y);
+
+		// add fusion rates to desintegration rates
+		r_included += nnet::fusion_to_desintegration_rates(fu, Y);
+		dr_included += nnet::fusion_to_desintegration_rates(dfu, Y);
+
+		// add temperature to the problem
+		auto RQ = include_temp(r_included, Q, rho);
+
+		// solve the system
+		solve_first_order(Y, T, RQ, dr_included, Q, dt)
+
+		// if needed, convert back to masses
+		mols_to_masses(Y, M);
+
+	------------------- */
+
+	namespace utils {
+		template<typename Float>
+		Eigen::SparseMatrix<Float> sparsify(const Eigen::MatrixXd &Min, const Float epsilon=1e-16) {
+			/* -------------------
+			put a "sparsified" version of Min into Mout according to epsilon 
+			------------------- */
+			std::vector<Eigen::Triplet<Float>> coefs;
+
+			for (int i = 0; i < Min.cols(); ++i)
+				for (int j = 0; j < Min.rows(); ++j)
+					if (std::abs(Min(i, j)) > epsilon)
+						coefs.push_back(Eigen::Triplet<Float>(i, j, Min(i, j)));
+
+			Eigen::SparseMatrix<Float> Mout(Min.cols(), Min.rows());
+			Mout.setFromTriplets(coefs.begin(), coefs.end());
+			return Mout;
+		}
+	}
+
+	template<class vector>
+	void masses_to_mols(vector &X, const vector &M) {
+		/* -------------------
+		goes from a vector of masses to a vector of mol
+
+		inverse of mols_to_masses
+		------------------- */
+
+		const int dimension = X.size();
+		for (int i = 0; i < dimension; ++i)
+			X(i) /= M(i);
+	}
+
+	template<class vector>
+	void mols_to_masses(vector &Y, const vector &M) {
+		/* -------------------
+		goes from a vector of masses to a vector of mol
+
+		inverse of masses_to_mols
+		------------------- */
+
+		const int dimension = Y.size();
+		for (int i = 0; i < dimension; ++i)
+			Y(i) *= M(i);
+	}
+
+	template<class matrix>
+	matrix desintegration_rate_to_first_order(matrix &r) {
+		/* -------------------
+		simply add the diagonal desintegration terms to the desintegration rates if not included
+
+		makes sure that the equation ends up being : dY/dt = r*Y
+		------------------- */
+
+		int dimension = r.cols();
+		matrix r_out = r;
+
+		for (int i = 0; i < dimension; ++i) {
+			r_out(i, i) = 0;
+			for (int j = 0; j < dimension; ++j) 
+				if (j != i)
+					r_out(i, i) -= r(j, i);
+		}
+
+		return r_out;
+	}
+
+	template<class tensor, class vector>
+	Eigen::MatrixXd fusion_to_desintegration_rates(const tensor &fu, const vector &Y) {
+		/* -------------------
+		include fusion rate into desintegration rates for a given state Y
+		------------------- */
+
+		const int dimension = Y.size();
+		Eigen::MatrixXd r_out = Eigen::MatrixXd::Zero(dimension, dimension);
+
+		// add fusion rates
+		for (int i = 0; i < dimension; ++i)
+			for (int j = 0; j < dimension; ++j)
+				if (i != j) {
+					// add i + i -> j
+					r_out(j, i) += fu(j, i, i)*Y(i);
+					r_out(i, i) -= fu(j, i, i)*Y(i)*2;
+
+					// add i + k -> j
+					for (int k = 0; k < dimension; ++k)
+						if (i != k) {
+							r_out(j, i) += (fu(j, i, k) + fu(j, k, i))*Y(k)/2;
+							r_out(i, i) -= (fu(j, i, k) + fu(j, k, i))*Y(k);
+						}
+				}
+
+		return r_out;
+	}
+
+	template<class tensor>
+	Eigen::MatrixXd fission_to_desintegration_rates(const tensor &fi) {
+		/* -------------------
+		include fission rate into desintegration rates
+		------------------- */
+
+		const int dimension = fi.dimension(0);
+		Eigen::MatrixXd r_out = Eigen::MatrixXd::Zero(dimension, dimension);
+
+		// add fission rates
+		for (int i = 0; i < dimension; ++i)
+			for (int j = 0; j < dimension; ++j)
+				if (i != j) {
+					// add i -> j + j
+					r_out(j, i) += fi(j, j, i)*2;
+					r_out(i, i) -= fi(j, j, i);
+
+					// add i -> j + k
+					for (int k = 0; k < dimension; ++k)
+						if (j != k) {
+							r_out(j, i) += fi(j, k, i) + fi(k, j, i);
+							r_out(i, i) -= (fi(j, k, i) + fi(k, j, i))/2;
+						}
+				}
+
+		return r_out;
+	}
+
+	template<class matrix, class tensor, class vector>
+	matrix fusion_to_desintegration_rates(matrix &r, const tensor &f, const vector &Y) {
+		/* -------------------
+		include fusion rate into desintegration rates for a given state Y
+		------------------- */
+
+		const int dimension = Y.size();
+		matrix r_out = Eigen::MatrixXd::Zero(dimension, dimension);
+
+		// add fusion rates
+		for (int i = 0; i < dimension; ++i)
+			for (int j = 0; j < dimension; ++j)
+				if (i != j) {
+					// add i + i -> j
+					r_out(j, i) += f(j, i, i)*Y(i);
+
+					// add i + k -> j
+					for (int k = 0; k < dimension; ++k)
+						if (i != k) {
+							r_out(j, i) += (f(j, i, k) + f(j, k, i))*Y(k)/2;
+							r_out(i, i) -= (f(j, i, k) + f(j, k, i))*Y(k);
+						}
+				}
+
+		return r_out;
+	}
+
+	template<class matrix, class vector, typename Float>
+	Eigen::MatrixXd include_temp(const matrix &r, const vector &Q, const Float rho) {
+		/* -------------------
+		add a row to r based on Q so that, d{Y, T}/dt = r*Y
+		------------------- */
+
+		const int dimension = Q.size();
+		Eigen::MatrixXd RQ(dimension + 1, dimension);
+
+		// insert r
+		RQ(Eigen::seq(1, dimension), Eigen::seq(0, dimension - 1)) = r;
+
+		// temperature terms
+		for (int i = 0; i < dimension; ++i) {
+			RQ(0, i) = -r(i, i)*Q(i)/rho;
+
+			// add i -> j
+			for (int j = 0; j < dimension; ++j) 
+				if (i != j)
+					RQ(0, i) += -r(j, i)*Q(j)/rho;
+		}
+
+		return RQ;
+	}
+
+	template<class matrix, class vector, typename Float>
+	void solve_first_order(vector &Y, Float &T, const matrix &RQ, const matrix &dr, const Float dt, const Float theta=1, const Float epsilon=1e-16) {
+		/* -------------------
+		Solves d{Y, T}/dt = RQ*Y using eigen:
+		{DY, DT} = RQ*(Y + teta*DY)*dt + (dr*Y*DT, 0)*dt
+		=> {DY, DT} * (I - dt*teta*{RQ, dr*Y)} = dt*{r, Q}*Y
+		------------------- */
+
+		const int dimension = Y.size();
+
+		// right hand side
+		const vector RHS = RQ*Y*dt;
+
+		// left hand side matrix (M)
+		matrix M(dimension + 1, dimension + 1);
+		M(Eigen::seq(0, dimension), Eigen::seq(1, dimension)) = RQ;
+		M(Eigen::seq(1, dimension), 0) = dr*Y;
+
+		M *= -theta*dt;
+		M += Eigen::MatrixXd::Identity(dimension + 1, dimension + 1);
+
+		// sparcify M
+		auto sparse_M = utils::sparsify(M, epsilon);
+
+		/* -------------------
+		TODO: Add loop
+		------------------- */
+		// now solve {Dy, DT}*M = RHS
+		Eigen::BiCGSTAB<Eigen::SparseMatrix<Float>>  BCGST;
+		BCGST.compute(sparse_M);
+		auto const DY_T = BCGST.solve(RHS);
+
+		// add to solution
+		T += DY_T(0);
+		Y += DY_T(Eigen::seq(1, dimension));
+	}
+
+
+	namespace net14 {
+		template<class matrix, typename Float>
+		void get_net14_desintegration_rates(matrix &r, const Float T) {
+			/* -------------------
+			simply copute desintegration rates within net-14
+			------------------- */
+
+			// TODO
+		}
+
+		template<class matrix, typename Float>
+		void get_net14_desintegration_rates_derivatives(matrix &dr, const Float T) {
+			/* -------------------
+			simply copute desintegration rates derivatives within net-14
+			------------------- */
+
+			// TODO
+		}
+
+		template<class tensor, typename Float>
+		void get_net14_fusion_rates(tensor &f, const Float T) {
+			/* -------------------
+			simply copute fusion rates within net-14
+			------------------- */
+
+			// TODO
+		}
+
+		template<class tensor, typename Float>
+		void get_net14_fusion_rates_derivatives(tensor &df, const Float T) {
+			/* -------------------
+			simply copute fusion rates derivatives within net-14
+			------------------- */
+
+			// TODO
+		}
+	}
+
+	namespace net87 {
+		template<class matrix, typename Float>
+		void get_net87_desintegration_rates(matrix &r, const Float T) {
+			/* -------------------
+			simply copute desintegration rates within net-14
+			------------------- */
+
+			// TODO
+		}
+
+		template<class matrix, typename Float>
+		void get_net87_desintegration_rates_derivatives(matrix &dr, const Float T) {
+			/* -------------------
+			simply copute desintegration rates derivatives within net-14
+			------------------- */
+
+			// TODO
+		}
+
+		template<class tensor, typename Float>
+		void get_net87_fusion_rates(tensor &f, const Float T) {
+			/* -------------------
+			simply copute fusion rates within net-14
+			------------------- */
+
+			// TODO
+		}
+
+		template<class tensor, typename Float>
+		void get_net87_fusion_rates_derivatives(tensor &df, const Float T) {
+			/* -------------------
+			simply copute fusion rates derivatives within net-14
+			------------------- */
+
+			// TODO
+		}
+	}
+}
---
+++ nuclear-net-test.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include "nuclear-net-mockup.hpp"
+
+int main() {
+	// photodesintegration rates
+	Eigen::MatrixXd r(3,3);
+	Eigen::MatrixXd dr(3,3);
+	dr(1,0) = 0.1;
+	dr(0,1) = -0.2;
+
+	// fusion rates
+	Eigen::Tensor<float, 3> fu(3, 3, 3);
+	Eigen::Tensor<float, 3> dfu(3, 3, 3);
+	dfu(2, 0, 0) = 0.2;
+	dfu(2, 0, 1) = 0.1;
+	dfu(2, 1, 1) = 0.2;
+
+	// fission rates
+	Eigen::Tensor<float, 3> fi(3, 3, 3);
+	Eigen::Tensor<float, 3> dfi(3, 3, 3);
+	dfi(0, 0, 2) = 0.01;
+	dfi(0, 1, 2) = 0.03;
+
+	// mass excedents
+	Eigen::VectorXd Q(3);
+	Q(0) = 0.4;
+	Q(1) = 0.3;
+	Q(2) = 0.2;
+
+	// molar masses
+	Eigen::VectorXd M(3);
+	M(0) = 2;
+	M(1) = 2;
+	M(2) = 4;
+
+	// initial state
+	Eigen::VectorXd Y(3);
+	double T = 1;
+	Y(0) = 0.8;
+	Y(1) = 0.7;
+	Y(2) = 0.2;
+
+	std::cout << Y.transpose() << ", " << T << std::endl;
+
+	auto last_Y = Y;
+	double last_T = T;
+	double E_tot, E_tot_0 = Y.dot(Q) + T;
+	double m_tot, m_tot_0 = Y.dot(M);
+
+	double dt=1e-3, T_max = 2;
+	const int n_print = 20;
+	for (int i = 0; i < T_max/dt; ++i) {
+		fu(2, 0, 0) = 0.7 + dfu(2, 0, 0)*(T - 1);
+		fu(2, 0, 1) = 0.6 + dfu(2, 0, 1)*(T - 1);
+		fu(2, 1, 1) = 0.4 + dfu(2, 1, 1)*(T - 1);
+
+		fi(0, 0, 2) = 0.1 + dfi(0, 0, 2)*(T - 1);
+		fi(0, 1, 2) = 0.05 + dfi(0, 1, 2)*(T - 1);
+
+		r(0,1) = 0.8 + dr(0,1)*(T - 1);
+		r(1,0) = 0.7 + dr(1,0)*(T - 1);
+
+		//to insure that the equation is dY/dt = r*Y
+		auto r_included = nnet::desintegration_rate_to_first_order(r);
+		auto dr_included = nnet::desintegration_rate_to_first_order(dr);
+
+		// add fission rates to desintegration rates
+		r_included += nnet::fission_to_desintegration_rates(fi);
+		dr_included += nnet::fission_to_desintegration_rates(dfi);
+
+		// add fusion rates to desintegration rates
+		r_included += nnet::fusion_to_desintegration_rates(fu, Y);
+		dr_included += nnet::fusion_to_desintegration_rates(dfu, Y);
+
+		// add temperature to the problem
+		auto RQ = nnet::include_temp(r_included, Q, 1);
+
+		// solve the system
+		nnet::solve_first_order(Y, T, RQ, dr_included, dt, 0.6);
+
+		E_tot = Y.dot(Q) + T;
+		m_tot = Y.dot(M);
+
+		if (i % (int)((float)T_max / (dt*(float)n_print)) == 0)
+			std::cout << Y.transpose() << ",\t(E_tot=" << E_tot << ",\tDelta_E_tot=" << E_tot_0 - E_tot << "),\t(m_tot=" << m_tot << ",\tDelta_m_tot=" << m_tot_0 - m_tot << "),\t" << T << "\n"; //",\t(" << Q.dot(Y - last_Y) << " ?= "<< (T - last_T) << ")\n";
+
+		if (i != T_max/dt - 1) {
+			last_Y = Y;
+			last_T = T;
+		}
+	}
+
+	std::cout << Y.transpose() << ",\t(E_tot=" << E_tot << ",\tDelta_E_tot=" << E_tot_0 - E_tot << "),\t(m_tot=" << m_tot << ",\tDelta_m_tot=" << m_tot_0 - m_tot << "),\t" << T << "\n"; //",\t(" << Q.dot(Y - last_Y) << " ?= "<< (T - last_T) << ")\n";
+}
