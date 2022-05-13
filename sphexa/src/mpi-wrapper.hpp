cf76aa2a93d3de3cfc6605fa99bea07ce2d0b284
jolatechno <joseph.touzet@gmail.com>
2022-05-13 10:56:05 +0200

    implemented MPI functions, will test them later

--- sphexa/src/mpi-wrapper.hpp
+++ sphexa/src/mpi-wrapper.hpp
@@ -19,24 +19,24 @@ namespace sphexa::mpi {
 		const std::vector<std::size_t> *particule_id;
 
 		// send partition limits
-		std::vector<int> send_begin;
+		std::vector<int> send_disp;
 		std::vector<int> send_count;
 
 		// send partition
 		std::vector<std::size_t> send_partition;
 
 		// send partition limits
-		std::vector<int> recv_begin;
+		std::vector<int> recv_disp;
 		std::vector<int> recv_count;
 
 		// send partition
 		std::vector<std::size_t> recv_partition;
 
 		void resize_comm_size(const int size) {
-			send_begin.resize(size + 1, 0);
+			send_disp.resize(size + 1, 0);
 			send_count.resize(size    , 0);
 
-			recv_begin.resize(size + 1, 0);
+			recv_disp.resize(size + 1, 0);
 			recv_count.resize(size    , 0);
 		}
 
@@ -59,23 +59,34 @@ namespace sphexa::mpi {
 		mpi_partition partition;
 
 		// prepare vector sizes
+		const int n_particles = node_id.size();
 		partition.resize_comm_size(size);
-		partition.resize_num_particles(node_id.size());
+		partition.resize_num_particles(n_particles);
 
 		// intialize references
 		partition.node_id = &node_id;
 		partition.particule_id = &particule_id;
 
 		// localy partition
-		/* TODO */
+		utils::parallel_generalized_partition_from_iota(partition.send_partition.begin(), partition.send_partition.end(), 0, 
+			partition.send_disp.begin(), partition.send_disp.end(),
+			[&](const int idx) {
+				return node_id[idx];
+			});
 
 		// send counts
-		std::adjacent_difference(partition.send_begin.begin() + 1, partition.send_begin.end(), partition.send_count.begin());
-		/* TODO */
-		std::partial_sum(partition.recv_count.begin(), partition.recv_count.end(), partition.recv_begin.begin() + 1);
+		std::adjacent_difference(partition.send_disp.begin() + 1, partition.send_disp.end(), partition.send_count.begin());
+		MPI_Alltoall(&partition.send_count[0], 1, MPI_INT, &partition.recv_count[0], 1, MPI_INT, MPI_COMM_WORLD);
+		std::partial_sum(partition.recv_count.begin(), partition.recv_count.end(), partition.recv_disp.begin() + 1);
 
 		// send particle id
-		/* TODO */
+		// prepare send buffer
+		#pragma omp parallel for schedule(static)
+		for (size_t i = 0; i < n_particles; ++i)
+			partition.recv_partition[i] = particule_id[partition.send_partition[i]];
+		// send particle id
+		MPI_Alltoallv(&partition.recv_partition[0], &partition.send_count[0], &partition.send_disp[0], MPI_INT,
+					   MPI_IN_PLACE,                &partition.recv_count[0], &partition.recv_disp[0], MPI_INT, MPI_COMM_WORLD);
 
 		return partition;
 	}
@@ -85,8 +96,24 @@ namespace sphexa::mpi {
 	 * TODO
 	 */
 	template<typename T>
-	void direct_sync_data_from_partition(const mpi_partition &partition, const std::vector<T> &send_vector, const std::vector<T> &recv_vector, const MPI_Datatype datatype) {
-		/* TODO */
+	void direct_sync_data_from_partition(const mpi_partition &partition, const std::vector<T> &send_vector, std::vector<T> &recv_vector, const MPI_Datatype datatype) {
+		// prepare send buffer
+		const int n_particles = partition.node_id->size();
+		std::vector<T> send_recv_buffer(n_particles);
+
+		// prepare (partition) buffer
+		#pragma omp parallel for schedule(static)
+		for (size_t i = 0; i < n_particles; ++i)
+			send_recv_buffer[i] = send_vector[partition.send_partition[i]];
+
+		// send buffer
+		MPI_Alltoallv(&send_recv_buffer[0], &partition.send_count[0], &partition.send_disp[0], datatype,
+					   MPI_IN_PLACE,        &partition.recv_count[0], &partition.recv_disp[0], datatype, MPI_COMM_WORLD);
+
+		// reconstruct (un-partition) vector from buffer
+		#pragma omp parallel for schedule(static)
+		for (size_t i = 0; i < n_particles; ++i)
+			recv_vector[i] = send_vector[partition.recv_partition[i]];
 	}
 
 	/// function that sync data from detached data
@@ -94,7 +121,25 @@ namespace sphexa::mpi {
 	 * TODO
 	 */
 	template<typename T>
-	void reversed_sync_data_from_partition(const mpi_partition &partition, const std::vector<T> &send_vector, const std::vector<T> &recv_vector, const MPI_Datatype datatype) {
-		/* TODO */
+	void reversed_sync_data_from_partition(const mpi_partition &partition, const std::vector<T> &send_vector, std::vector<T> &recv_vector, const MPI_Datatype datatype) {
+		// exact same thing as "direct_sync_data_from_partition" but with "send_ <-> recv_"
+
+		// prepare send buffer
+		const int n_particles = partition.node_id->size();
+		std::vector<T> send_recv_buffer(n_particles);
+
+		// prepare (partition) buffer
+		#pragma omp parallel for schedule(static)
+		for (size_t i = 0; i < n_particles; ++i)
+			send_recv_buffer[partition.recv_partition[i]] = send_vector[i];
+
+		// send buffer
+		MPI_Alltoallv(&send_recv_buffer[0], &partition.send_count[0], &partition.send_disp[0], datatype,
+					   MPI_IN_PLACE,        &partition.recv_count[0], &partition.recv_disp[0], datatype, MPI_COMM_WORLD);
+
+		// reconstruct (un-partition) vector from buffer
+		#pragma omp parallel for schedule(static)
+		for (size_t i = 0; i < n_particles; ++i)
+			recv_vector[partition.recv_partition[i]] = send_vector[i];
 	}
 }
--- sphexa/src/utils/algorithm.hpp
+++ sphexa/src/utils/algorithm.hpp
@@ -1,4 +1,86 @@
 #pragma once
 
-/* mostly stolen from QuIDS */
-/* TODO */
+#include <vector>
+#include <parallel/algorithm>
+#include <parallel/numeric>
+
+#include <omp.h>
+
+/*
+Simple utilities
+stolen from QuIDS (https://github.com/jolatechno/QuIDS)
+*/
+
+namespace utils {
+	/// parallel iota
+	template <class iteratorType, class valueType>
+	void parallel_iota(iteratorType begin, iteratorType end, const valueType value_begin) {
+		size_t distance = std::distance(begin, end);
+
+		if (value_begin == 0) {
+			#pragma omp parallel for 
+			for (size_t i = 0; i < distance; ++i)
+				begin[i] = i;
+		} else
+			#pragma omp parallel for 
+			for (size_t i = 0; i < distance; ++i)
+				begin[i] = value_begin + i;
+	}
+
+	/// linear partitioning algorithm into n partitions without an initialized index list in parallel
+	template <class idIteratorType, class idType, class countIteratorType, class functionType>
+	void parallel_generalized_partition_from_iota(idIteratorType idx_in, idIteratorType idx_in_end, idType const iotaOffset,
+		countIteratorType offset, countIteratorType offset_end,
+		functionType const partitioner) {
+
+		int const n_segment = std::distance(offset, offset_end) - 1;
+		long long int const id_end = std::distance(idx_in, idx_in_end);
+
+		/* limit values */
+		offset[0] = 0;
+		offset[n_segment] = id_end;
+
+		if (n_segment == 1)
+			return;
+		if (id_end == 0) {
+			std::fill(offset, offset_end, 0);
+			return;
+		}
+
+		/* number of threads */
+		int num_threads;
+		#pragma omp parallel
+		#pragma omp single
+		num_threads = omp_get_num_threads();
+
+		std::vector<size_t> count(n_segment*num_threads, 0);
+
+		#pragma omp parallel
+		{
+			int thread_id = omp_get_thread_num();
+
+			#pragma omp for 
+			for (long long int i = id_end + iotaOffset - 1; i >= iotaOffset; --i) {
+				auto key = partitioner(i);
+				++count[key*num_threads + thread_id];
+			}
+		}
+		
+		__gnu_parallel::partial_sum(count.begin(), count.begin() + n_segment*num_threads, count.begin());
+
+		#pragma omp parallel
+		{
+			int thread_id = omp_get_thread_num();
+			
+			#pragma omp for 
+			for (long long int i = id_end + iotaOffset - 1; i >= iotaOffset; --i) {
+				auto key = partitioner(i);
+				idx_in[--count[key*num_threads + thread_id]] = i;
+			}
+		}
+
+		#pragma omp parallel for 
+		for (int i = 1; i < n_segment; ++i)
+			offset[i] = count[i*num_threads];
+	}
+}
