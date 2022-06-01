e8cf8fb1f9afe78dc5d6d96745c5879461b0d034
jolatechno <joseph.touzet@gmail.com>
2022-06-01 12:30:44 +0200

    started preping for CUDA solver

--- src/eigen/cudaSolver.hpp
+++ src/eigen/cudaSolver.hpp
@@ -4,5 +4,40 @@ namespace eigen::cudasolver {
 	/// batch size for the GPU batched solver
 	size_t batch_size = 1000000;
 
-	/* TODO */
+	/// batch GPU solver
+	/**
+	 * TODO
+	 */
+	template<typename Float>
+	class batch_solver {
+	private:
+		int dimension;
+	
+	public:
+		/// allocate buffers for batch solver
+		batch_solver(int dimension_) : dimension(dimension_) {
+			/* TODO */
+		}
+
+		/// insert system into batch solver
+		void insert_system(size_t i, const Float* M, const Float *RHS) {
+			/* TODO */
+
+			//throw std::runtime_error("CUDA batched solver \"insert_system\" not yet implemented !");
+		}
+
+		/// solve systems
+		void solve() {
+			/* TODO */
+
+			throw std::runtime_error("CUDA batched solver \"solve\" not yet implemented !");
+		}
+
+		/// retrieve results
+		void get_res(size_t i, Float *res) {
+			/* TODO */
+
+			throw std::runtime_error("CUDA batched solver \"get_res\" not yet implemented !");
+		}
+	};
 }
--- src/sphexa/nuclear-net.hpp
+++ src/sphexa/nuclear-net.hpp
@@ -28,17 +28,28 @@ namespace sphexa::sphnnet {
 	/**
 	 * TODO
 	 */
-	template<class Data, class func_rate, class func_BE, class func_eos, typename Float>
+	template<class Data, class func_rate, class func_BE, class func_eos, typename Float, class nseFunction=void*>
 	void computeNuclearReactions(Data &n, const Float hydro_dt, const Float previous_dt,
-		const std::vector<nnet::reaction> &reactions, const func_rate construct_rates, const func_BE construct_BE, const func_eos eos) {
+		const std::vector<nnet::reaction> &reactions, const func_rate construct_rates, const func_BE construct_BE, const func_eos eos,
+		const nseFunction jumpToNse=NULL)
+	{
 		const size_t n_particles = n.temp.size();
 
+#ifdef USE_CUDA
+		
+#endif
+
+		
+#ifndef USE_CUDA
+		/* !!!!!!!!!!!!!
+		simple CPU parallel solver
+		!!!!!!!!!!!!! */
 		#pragma omp parallel
 		{
+			
 			Float drho_dt;
 			auto Y_buffer = n.Y[0];
 
-#ifndef USE_CUDA
 			#pragma omp for schedule(dynamic)
 			for (size_t i = 0; i < n_particles; ++i) {
 				drho_dt = n.previous_rho[i] <= 0 ? 0. : (n.rho[i] - n.previous_rho[i])/previous_dt;
@@ -46,20 +57,48 @@ namespace sphexa::sphnnet {
 				if (n.rho[i] > nnet::constants::min_rho && n.temp[i] > nnet::constants::min_temp)
 					nnet::solve_system_substep(reactions, construct_rates, construct_BE, eos,
 						n.Y[i], n.temp[i], Y_buffer,
-						n.rho[i], drho_dt, hydro_dt, n.dt[i]);
+						n.rho[i], drho_dt, hydro_dt, n.dt[i],
+						jumpToNse);
 			}
 #else
+		/* !!!!!!!!!!!!!
+		GPU batch solver
+		!!!!!!!!!!!!! */
+		// intitialized bash solver data
+		const int dimension = n.Y[0].size();
+		eigen::cudasolver::batch_solver<Float> batch_solver(dimension);
+
+		#pragma omp parallel
+		{
+			Float drho_dt;
+
+			// buffers
+			std::vector<Float> vect_buffer(dimension + 1), mat_buffer((dimension + 1)*(dimension + 1));
+
 			// get number of thread and thread number
 			const int num_threads = omp_get_num_threads();
 			const int thread_id   = omp_get_thread_num();
 
 			// initialize limits
-			const size_t particleBegin = n_particles                  *thread_id      /num_threads;
-			const size_t particleEnd   = n_particles                  *(thread_id + 1)/num_threads;
-			const size_t batchBegin    = eigen::cudasolver::batch_size*thread_id      /num_threads;
-			const size_t batchEnd      = eigen::cudasolver::batch_size*(thread_id + 1)/num_threads;
+			const size_t particleBegin     = n_particles                  *thread_id      /num_threads;
+			const size_t particleEnd       = n_particles                  *(thread_id + 1)/num_threads;
+			const size_t batchBegin        = eigen::cudasolver::batch_size*thread_id      /num_threads;
+			const size_t batchEnd          = eigen::cudasolver::batch_size*(thread_id + 1)/num_threads;
+			const size_t batchParticleSize = particleEnd - particleBegin;
 
-			throw std::runtime_error("CUDA batched solver not yet implemented !");
+			// data for batch initialization
+			std::vector<int>   iter(batchParticleSize, 0);
+			std::vector<bool>  finished(batchParticleSize, false);
+			std::vector<Float> elapsed_time(batchParticleSize, 0.);
+			std::vector<Float> T_buffer(batchParticleSize);
+			decltype(n.Y)      Y_buffer(batchParticleSize);
+
+			// intialize buffers
+			for (size_t i = particleBegin; i < particleEnd; ++i) {
+				T_buffer[i - particleBegin] = n.temp[i];
+				Y_buffer[i - particleBegin] = n.Y[i]; 
+			}
+
 
 			// solving loop
 			while (true) {
@@ -67,10 +106,26 @@ namespace sphexa::sphnnet {
 				size_t batchID = batchBegin;
 				for (size_t i = particleBegin; i < particleEnd; ++i)
 					if (n.rho[i] > nnet::constants::min_rho && n.temp[i] > nnet::constants::min_temp &&
-						/* TODO */ true)
+						!finished[i - particleBegin])
 					{
+						drho_dt = n.previous_rho[i] <= 0 ? 0. : (n.rho[i] - n.previous_rho[i])/previous_dt;
 
-						/* TODO */
+						// preparing system
+						++iter[i - particleBegin];
+						auto [Mp, RHS] = nnet::prepare_system_substep(
+							reactions, construct_rates, construct_BE, eos,
+							n.Y[i], n.temp[i], Y_buffer[i - particleBegin], T_buffer[i - particleBegin],
+							n.rho[i], drho_dt,
+							hydro_dt, elapsed_time[i - particleBegin], n.dt[i],
+							jumpToNse);
+
+						// copy to buffer then insert into batch solver
+						for (int i = 0; i <= dimension; ++i) {
+							vect_buffer[i] = RHS[i];
+							for (int j = 0; j <= dimension; ++j)
+								mat_buffer[(dimension+1)*i + j] = Mp(i, j);
+						}
+						batch_solver.insert_system(batchID, mat_buffer.data(), vect_buffer.data());
 
 						// break condition
 						++batchID;
@@ -79,13 +134,15 @@ namespace sphexa::sphnnet {
 					}
 
 				// batch solves
-				/* TODO */
+				#pragma omp barrier
+				#pragma omp master
+				batch_solver.solve();
 
 				// finalize
 				batchID = batchBegin;
 				for (size_t i = particleBegin; i < particleEnd; ++i)
 					if (n.rho[i] > nnet::constants::min_rho && n.temp[i] > nnet::constants::min_temp &&
-						/* TODO */ true)
+						!finished[i - particleBegin])
 					{
 
 						/* TODO */
