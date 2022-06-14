ff663be80a6fe695fe9a0650a9a44ded382c3f46
jolatechno <joseph.touzet@gmail.com>
2022-06-14 15:29:03 +0200

    trying to fix CUDA

--- src/sphexa/CUDA/nuclear-net.cu
+++ src/sphexa/CUDA/nuclear-net.cu
@@ -13,32 +13,38 @@
 
 namespace nnet {
 namespace constants {
-	extern double min_rho, min_temp;
+	extern __device__ double dev_min_rho, dev_min_temp;
 }
-	
+
+extern "C" {
 	// forward definition
-	template<class func_type, class func_eos, typename Float, class nseFunction>
+	// template<class func_type /*, class func_eos*/ /*, typename Float*/>
 	__host__ __device__ void solve_system_substep(const int dimension,
 		Float *Mp, Float *RHS, Float *DY_T, Float *rates, Float *drates_dT,
 		const ptr_reaction_list &reactions, const func_type &construct_rates_BE, const func_eos &eos,
 		Float *final_Y, Float &final_T, Float *Y_buffer,
-		const Float final_rho, const Float drho_dt, Float const dt_tot, Float &dt,
-		const nseFunction jumpToNse=NULL);
+		const Float final_rho, const Float drho_dt, Float const dt_tot, Float &dt);
+}
 }
 
 namespace sphexa {
 namespace sphnnet {
-	template<class func_type, class func_eos, typename Float>
-	__global__ void cudaKernelComputeNuclearReactions(int n_particles, int dimension,
+	// template<class func_type /*, class func_eos*/ /*, typename Float*/>
+	__global__ void cudaKernelComputeNuclearReactions(const int n_particles, const int dimension,
 	Float *rho_, Float *previous_rho_, Float *Y_, Float *temp_, Float *dt_,
 	const Float hydro_dt, const Float previous_dt,
 		const nnet::ptr_reaction_list &reactions, const func_type &construct_rates_BE, const func_eos &eos)
 	{
-		Float Mp[(dimension + 1)*(dimension + 1)], RHS[dimension + 1], DY_T[dimension + 1], rates[reactions.num_reactions], drates_dT[reactions.num_reactions], Y_buffer[dimension];
-
 	    const int i = blockIdx.x*blockDim.x + threadIdx.x;
-	    if (i < n_particles)
-		    if (rho_[i] > nnet::constants::min_rho && temp_[i] > nnet::constants::min_temp) {
+	    if (i < n_particles) {
+	    	Float *Mp        = (Float*)malloc((dimension + 1)*(dimension + 1)*sizeof(Float));
+			Float *RHS       = (Float*)malloc(                (dimension + 1)*sizeof(Float));
+			Float *DY_T      = (Float*)malloc(                (dimension + 1)*sizeof(Float));
+			Float *Y_buffer  = (Float*)malloc(                      dimension*sizeof(Float));
+			Float *rates     = (Float*)malloc(reactions.num_reactions*sizeof(Float));
+			Float *drates_dT = (Float*)malloc(reactions.num_reactions*sizeof(Float));
+
+		    if (rho_[i] > nnet::constants::dev_min_rho && temp_[i] > nnet::constants::dev_min_temp) {
 				// compute drho/dt
 				Float drho_dt = previous_rho_[i] <= 0 ? 0. : (rho_[i] - previous_rho_[i])/previous_dt;
 
@@ -49,10 +55,18 @@ namespace sphnnet {
 					&Y_[dimension*i], temp_[i], Y_buffer,
 					rho_[i], drho_dt, hydro_dt, dt_[i]);
 			}
+
+			free(Mp);
+			free(RHS);
+			free(DY_T);
+			free(Y_buffer);
+			free(rates);
+			free(drates_dT);
+		}
 	}
 
-	template<class func_type, class func_eos, typename Float>
-	__host__ void cudaComputeNuclearReactions(int n_particles, int dimension,
+	// template<class func_type /*, class func_eos*/ /*, typename Float*/>
+	__host__ void cudaComputeNuclearReactions(const int n_particles, const int dimension,
 	Float *rho_, Float *previous_rho_, Float *Y_, Float *temp_, Float *dt_,
 	const Float hydro_dt, const Float previous_dt,
 		const nnet::ptr_reaction_list &reactions, const func_type &construct_rates_BE, const func_eos &eos)
--- src/sphexa/CUDA/nuclear-net.cuh
+++ src/sphexa/CUDA/nuclear-net.cuh
@@ -1,14 +1,24 @@
 #pragma once
 
+using Float      = double;
+using eos_struct = struct eos_output {
+			Float cv, dP_dT, p;
+			Float cp, c, u;
+	};
+using func_eos   = eos_struct (*) (const Float *, const Float, const Float);
+using func_type  = void     (*) (const Float *, const Float, const Float, eos_struct, const Float *, const Float *, const Float *);
+
+extern "C" {
 namespace nnet {
 	// forward definition
 	class ptr_reaction_list;
 }
+}
 
 namespace sphexa {
 namespace sphnnet {
-	template<class func_type, class func_eos, typename Float>
-	__host__ void cudaComputeNuclearReactions(int n_particles, int dimension,
+	// template<class func_type, class func_eos, typename Float>
+	__host__ void cudaComputeNuclearReactions(const int n_particles, const int dimension,
 	Float *rho_, Float *previous_rho_, Float *Y_, Float *temp_, Float *dt_,
 	const Float hydro_dt, const Float previous_dt,
 		const nnet::ptr_reaction_list &reactions, const func_type &construct_rates_BE, const func_eos &eos);
